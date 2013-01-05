4180adec42e2406f2b3b66c13f9c9313da8cc1c8
David Alpert <david@spinthemoose.com>
2013-01-05 15:02:33 -0600

    import is added at the beginning of the document

--- src/RefactorPlus/RemoveQualifierAction.cs
+++ src/RefactorPlus/RemoveQualifierAction.cs
@@ -46,16 +46,50 @@ namespace RefactorPlus
 
             var qualifier = typeDeclaration.TypeName.Qualifier;
 
-            var textRange = qualifier.GetTreeTextRange();
-            TextRange range = new TextRange(textRange.StartOffset.Offset, textRange.EndOffset.Offset+1);
-            _provider.Document.ReplaceText(range, string.Empty);
+            ReplaceQualifier(qualifier, string.Empty);
+
+            TreeNodeVisitor visitor = new FindUsingsVisitor(qualifier.QualifiedName);
+            _provider.PsiFile.Accept(visitor);
+
+            var usingDirective = factory.CreateUsingDirective(qualifier.QualifiedName);
+            IUsingDirective anchor = null;
+            _provider.PsiFile.AddImportAfter(usingDirective, anchor);
 
             return null;
+        }
+
+        private void ReplaceQualifier(ITreeNode treeNode, string alias) 
+        {
+            var textRange = treeNode.GetTreeTextRange();
+            var fromOffset = textRange.StartOffset.Offset;
+            var toOffset = textRange.EndOffset.Offset;
+
+            if (string.IsNullOrWhiteSpace(alias))
+                toOffset += 1; // to remove the '.' character that is no longer needed.
+
+            var range = new TextRange(fromOffset, toOffset);
+
+            this._provider.Document.ReplaceText(range, string.Empty);
         }
 
         public override string Text
         {
             get { return "Remove qualifier"; }
+        }
+    }
+
+    public class FindUsingsVisitor : TreeNodeVisitor
+    {
+        private string qualifiedName;
+
+        public FindUsingsVisitor(string qualifiedName)
+        {
+            this.qualifiedName = qualifiedName;
+        }
+
+        public override void VisitNode(ITreeNode node)
+        {
+            base.VisitNode(node);
         }
     }
 }
